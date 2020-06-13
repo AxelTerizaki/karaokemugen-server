56339f64a3db567f5c4492f8f20e1f18a783ebbf
Aeden <florentius.b@gmail.com>
2020-06-14 01:17:17 +0200

    Add all fields

--- kmexplorer/components/KaraEdit.vue
+++ kmexplorer/components/KaraEdit.vue
@@ -7,7 +7,7 @@
           <input class="file-input" type="file" name="resume" />
           <span class="file-cta">
             <span class="file-icon">
-				<font-awesome-icon icon="upload" :fixed-width="true" />
+              <font-awesome-icon icon="upload" :fixed-width="true" />
             </span>
             <span class="file-label">Choose a file…</span>
           </span>
@@ -36,15 +36,167 @@
         <input class="input" type="text" placeholder="Text input" v-model="karaoke.title" />
       </div>
     </div>
-	 <div class="field">
+    <div class="field">
       <label class="label">Name</label>
-      <editable-tag-group :tagType="1" :params="karaoke.series"
-	  	v-on:change="(tags) => karaoke.series = tags"></editable-tag-group>
+      <editable-tag-group
+        :tagType="1"
+        :params="karaoke.series"
+        v-on:change="(tags) => karaoke.series = tags"
+      ></editable-tag-group>
     </div>
     <div class="field">
       <label class="label">Name</label>
-      <editable-tag-group v-bind:checkboxes="true" :tagType="3" :params="karaoke.songtypes" 
-	  	v-on:change="(tags) => karaoke.songtypes = tags"></editable-tag-group>
+      <editable-tag-group
+        v-bind:checkboxes="true"
+        :tagType="3"
+        :params="karaoke.songtypes"
+        v-on:change="(tags) => karaoke.songtypes = tags"
+      ></editable-tag-group>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <input class="input" type="number" placeholder="Text input" v-model="karaoke.songorder" />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          :tagType="5"
+          :params="karaoke.langs"
+          v-on:change="(tags) => karaoke.langs = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <input class="input" type="number" placeholder="Text input" v-model="karaoke.year" />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          :tagType="2"
+          :params="karaoke.singers"
+          v-on:change="(tags) => karaoke.singers = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          :tagType="8"
+          :params="karaoke.songwriters"
+          v-on:change="(tags) => karaoke.songwriters = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          :tagType="4"
+          :params="karaoke.creators"
+          v-on:change="(tags) => karaoke.creators = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          :tagType="6"
+          :params="karaoke.authors"
+          v-on:change="(tags) => karaoke.authors = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          v-bind:checkboxes="true"
+          :tagType="10"
+          :params="karaoke.families"
+          v-on:change="(tags) => karaoke.families = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          v-bind:checkboxes="true"
+          :tagType="13"
+          :params="karaoke.platforms"
+          v-on:change="(tags) => karaoke.platforms = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          v-bind:checkboxes="true"
+          :tagType="12"
+          :params="karaoke.genres"
+          v-on:change="(tags) => karaoke.genres = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          v-bind:checkboxes="true"
+          :tagType="11"
+          :params="karaoke.origins"
+          v-on:change="(tags) => karaoke.origins = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          v-bind:checkboxes="true"
+          :tagType="7"
+          :params="karaoke.misc"
+          v-on:change="(tags) => karaoke.misc = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <editable-tag-group
+          v-bind:checkboxes="true"
+          :tagType="9"
+          :params="karaoke.groups"
+          v-on:change="(tags) => karaoke.groups = tags"
+        ></editable-tag-group>
+      </div>
+    </div>
+    <div class="field">
+      <label>{{karaoke.created_at ? new Date(karaoke.created_at).toLocaleString() : null}}</label>
+    </div>
+    <div class="field">
+      <label>{{karaoke.modified_at ? new Date(karaoke.modified_at).toLocaleString() : null}}</label>
+    </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Text input" v-model="karaoke.comment" />
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <button class="button is-link">Submit</button>
+      </div>
     </div>
   </form>
 </template>
